64a1854082e8996b464f15d8f2085b42cc28aa54
saketh-24 <saketh1094@gmail.com>
2024-06-16 10:37:06 +0530

    created route model and controller for group therapy and support group

--- backend/routes/groupsRoute.js
+++ backend/routes/groupsRoute.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const { getGroupTherapySessions , createGroupTherapySession } = require("../controllers/groupTherapyController");
 const { getSupportGroupSessions , createSupportGroupSession } = require("../../controllers/supportGroupController");
 
-router.get("/", getGroupTherapySessions);
-router.post("/", createGroupTherapySession);
+router.route("/Grouptherapy").get(getGroupTherapySessions);
+router.post("/Grouptherapy", createGroupTherapySession);
 
-router.get("/", getSupportGroupSessions);
-router.post("/", createSupportGroupSession);
+router.get("/Supportgroup", getSupportGroupSessions);
+router.post("/Supportgroup", createSupportGroupSession);
 
 module.exports = router;
 
